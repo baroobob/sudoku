3686bccaf394d9daccc3504ccaef44a30387d008
Jim Bridgewater <jwbwater@gmail.com>
2014-10-31 10:11:42 -0700

    Got solver working.

--- sudoku.py
+++ sudoku.py
@@ -15,9 +15,7 @@ import random
 ################################################################################
 
 class Sudoku(object):
-    """
-    Sudoku class for the Insight Data Engineering coding challenge
-    """
+    """ Sudoku class for the Insight Data Engineering coding challenge """
 
     def __init__(self):
 
@@ -40,7 +38,7 @@ class Sudoku(object):
 
 
     def puzzle_from_csv(self, file_name):
-        """ Read in Sudoku puzzle from a file of comma separated values """
+        """ Read in a Sudoku puzzle from a file of comma separated values """
 
         error_message = file_name + \
         "does not appear to be a valid 9 by 9 Sudoku puzzle."
@@ -58,9 +56,41 @@ class Sudoku(object):
             exit(error_message)
         if len(data_list) != 9:
             exit(error_message)
-        for row in range(9):
-            for column in range(9):
-                self.add_value_to_puzzle(data_list[row][column], row, column)
+        self._puzzle = numpy.array(data_list)
+        self._solution = numpy.array(data_list)
+        self._update_possible_values()
+
+
+    def _update_possible_values(self):
+        """
+        Updates _possible_values and _possibilities
+        after changes to _solution.
+        """
+        for i in range(9):
+            for j in range(9):
+                possible_values = self._find_possible_values(i, j)
+                self._possible_values[i][j] = possible_values
+                self._possibilities[i][j] = len(possible_values)
+
+
+    def _find_possible_values(self, row, column):
+        """
+        Returns a list of the possible values a grid location can take without
+        duplicating any of the values in its row, column, or 3 by 3 region.
+        """
+        if self._solution[row, column] in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            possible_values = [self._solution[row, column]]
+        else:
+            possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            affected_locations = _affected_locations(row, column)
+            for i in range(9):
+                for j in range(9):
+                    if affected_locations[i, j] == 1:
+                        # remove values found in the row, column and region
+                        value = self._solution[i, j]
+                        if value in possible_values:
+                            possible_values.remove(value)
+        return possible_values
 
 
     def add_value_to_puzzle(self, value, row, column):
@@ -157,19 +187,24 @@ class Sudoku(object):
         return valid
 
 
-    def solve_puzzle(self):
+    def solve(self):
         """
-        Solve the Sudoku puzzle. Start by assigning the only possible value to
-        all empty positions that have only one possibility. Then update
-        possible_values and possibilities and call solve_puzzle again.
+        Solve the Sudoku puzzle. Start by searching for empty locations that
+        have only one possible value. If any are found, enter the only possible
+        value in those locations, update possible_values and try again. Stop
+        when no more are found.
         """
-
-        while self._there_are_empty_positions_with_one_possibility():
-            for row in range(9):
-                for column in range(9):
-                    if self._possibilities == 1:
-                        self._solution = self._possible_values[row][column][0]
-        self._update_possible_values()
+        try_again = True
+        while try_again:
+            try_again = False
+            for i in range(9):
+                for j in range(9):
+                    if self._solution[i][j] == 0:
+                        if self._possibilities[i][j] == 1:
+                            try_again = True
+                            only_possible_value = self._possible_values[i][j][0]
+                            self._solution[i][j] = only_possible_value
+            self._update_possible_values()
 
 
     def initialize_randomly(self):
@@ -186,23 +221,9 @@ class Sudoku(object):
 # Function definitions
 ################################################################################
 
-def _print_grid(grid_data, error_message):
-    """ Print a 9 by 9 grid of numbers """
-    if grid_data.size != 81:
-        exit(error_message)
-    sudoku_string = ""
-    for row in grid_data:
-        for entry in row:
-            sudoku_string += str(entry) + ","
-        # remove comma after the last entry in each row
-        sudoku_string = sudoku_string[0:-1] + "\n"
-    print sudoku_string
-
-
-def _grid_to_csv(grid_data, file_name, error_message):
+def _grid_to_csv_string(grid_data, error_message):
     """
-    Write a 9 by 9 grid of numbers
-    to a file of comma separated values
+    Make a string of comma separated values from a 9 by 9 grid of numbers
     """
     if grid_data.size != 81:
         exit(error_message)
@@ -212,9 +233,22 @@ def _grid_to_csv(grid_data, file_name, error_message):
             csv_string += str(entry) + ","
         # remove comma after the last entry in each row
         csv_string = csv_string[0:-1] + "\n"
+    return csv_string
+
+
+def _print_grid(grid_data, error_message):
+    """ Print a 9 by 9 grid of numbers """
+    print _grid_to_csv_string(grid_data, error_message)
+
+
+def _grid_to_csv(grid_data, file_name, error_message):
+    """
+    Write a 9 by 9 grid of numbers
+    to a file of comma separated values
+    """
     try:
         file_handle = open(file_name, 'w')
-        file_handle.write(csv_string)
+        file_handle.write(_grid_to_csv_string(grid_data, error_message))
     except:
         exit("There was a problem writing to " + file_name)
 
@@ -240,51 +274,18 @@ def _affected_locations(row, column):
     return affected_locations
 
 
-def _possible_values(grid_data, row_index, column_index):
-    """
-    Returns a list of the possible values a grid location can take without
-    duplicating any of the values in its row, column, or 3 by 3 region.
-    """
-
-    if grid_data[row_index, column_index] == 0:
-        possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    else:
-        possibilities = grid_data[row_index, column_index]
-    # remove the values in this row
-    row = grid_data[row_index]
-    for entry in row:
-        if entry in possibilities:
-            possibilities.remove(entry)
-
-    # remove the values in this column
-    column = numpy.transpose(grid_data)[column_index]
-    for entry in column:
-        if entry in possibilities:
-            possibilities.remove(entry)
-
-    # remove the values in this 3 by 3 region
-    regions = list(
-        grid_data[i:i + 3, j:j + 3] for i in [0, 3, 6] for j in [0, 3, 6])
-    region_index = 3*(row_index/3) + column_index/3
-    region = regions[region_index]
-    region_entries = region.reshape(1, 9)[0]
-    for entry in region_entries:
-        if entry in possibilities:
-            possibilities.remove(entry)
-
-    print possibilities
-    return possibilities
-
-
-# write a Sudoku puzzle class that contains the following methods:
+# Sudoku puzzle class
+#
+# Already written:
 #     a method for reading csv files
 #     a method for writing csv files
 #     a method for printing Sudoku puzzles
 #     a method for testing the validity of proposed solutions
-
+#     a method to determine possible values for a position in the puzzle
+#     a method for solving puzzles
+#
+# Not yet written:
 #     a method for generating puzzles
-#         a method to determine possible values for a position in the puzzle
-#     a method for proposing solutions
 
 
 
@@ -295,7 +296,6 @@ def _possible_values(grid_data, row_index, column_index):
 if __name__ == '__main__':
 
     PUZZLE_FILE_NAME = "insight_sudoku_puzzle.csv"
-    SOLUTION_FILE_NAME = "insight_sudoku_solution.csv"
     sudoku_puzzle = Sudoku()
     sudoku_puzzle.puzzle_from_csv(PUZZLE_FILE_NAME)
     sudoku_puzzle.print_puzzle()
@@ -303,16 +303,18 @@ if __name__ == '__main__':
     print
     sudoku_puzzle.print_solution()
     print sudoku_puzzle._valid_solution()
+    print
+    sudoku_puzzle.solve()
+    sudoku_puzzle.print_solution()
+    print sudoku_puzzle._possibilities
+    print
+    print sudoku_puzzle._valid_solution()
+    sudoku_puzzle.write_solution_to_csv("my_solution.csv")
     exit()
 
     sudoku_puzzle.initialize_randomly()
     sudoku_puzzle.print_puzzle()
     exit()
 
-    sudoku_solution = Sudoku()
-    sudoku_solution.puzzle_from_csv(SOLUTION_FILE_NAME)
-    sudoku_puzzle.print_puzzle()
-    sudoku_solution.print_puzzle()
-    sudoku_solution.write_solution_to_csv("my_solution.csv")
 
 
